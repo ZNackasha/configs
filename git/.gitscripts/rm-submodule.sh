7afb327e2103a10bc9b9bb534d18462e76fd04a5
Gilgamesh22 <zaidnackasha22@gmail.com>
2020-01-28 06:21:12 -0500

    Update rm-submodule.sh

--- git/.gitscripts/rm-submodule.sh
+++ git/.gitscripts/rm-submodule.sh
@@ -1,4 +1,3 @@
-echo "removing submodule $1"
-git rm "$1"
+git submodule deinit -f -- $1
 rm -rf ".git/modules/$1"
-git config -f ".git/config" --remove-section "submodule.$1" 2> /dev/null
+git rm -f "$1"
